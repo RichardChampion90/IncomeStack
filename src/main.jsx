ee13db87af004203725698cae919b556e8cf3487
RichardChampion90 <richard.champion90@gmail.com>
2026-07-17 07:32:49 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,13 @@
 import React from "react";
-import ReactDOM from "react-dom/client"; import RetirementPlanner from "./RetirementPlanner.jsx";
+import ReactDOM from "react-dom/client"; import { BrowserRouter } from "react-router";
+
+import App from "./App.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RetirementPlanner />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
+
