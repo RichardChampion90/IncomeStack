0c8f2b98d086ba609de70fa8c5a4ac97c56add06
RichardChampion90 <richard.champion90@gmail.com>
2026-07-20 21:58:40 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import Header from "./Header";
 import IntroSection from "./IntroSection";
 import BenefitsSection from "./BenefitsSection";
 import HowItWorksSection from "./HowItWorksSection";
+import FinalCTASection from "./FinalCTASection";
+import Footer from "./Footer";
 
 function App() {
   function handleGetStarted() {
@@ -16,7 +18,10 @@ function App() {
         <IntroSection onGetStarted={handleGetStarted} />
         <BenefitsSection />
         <HowItWorksSection />
+        <FinalCTASection onGetStarted={handleGetStarted} />
       </main>
+
+      <Footer />
     </>
   );
 }
